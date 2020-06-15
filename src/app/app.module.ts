6e7f72997955037358d769884e55080dc7fa9d31
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-06-15 20:19:34 -0300

    Adicionando diálogo de confirmação antes da exclusão

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { ToastyModule } from 'ng2-toasty';
+import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog';
+import { ConfirmationService } from 'primeng/components/common/api';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -22,12 +24,17 @@ import { PessoaService } from './pessoas/pessoa.service';
     HttpModule,
 
     ToastyModule.forRoot(),
+    ConfirmDialogModule,
 
     CoreModule,
     LancamentosModule,
     PessoasModule
   ],
-  providers: [LancamentoService, PessoaService],
+  providers: [
+    LancamentoService,
+    PessoaService,
+    ConfirmationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
