6256c17fe9f85dfda99fdd481eb5910f674cc0d8
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-06-23 17:04:04 -0300

    Desafio: implementando o cadastro de pessoas

--- src/app/pessoas/pessoa.service.ts
+++ src/app/pessoas/pessoa.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 
 import 'rxjs/add/operator/toPromise';
 
+import { Pessoa } from 'app/core/model';
+
 export class PessoaFiltro {
   nome: string;
   pagina = 0;
@@ -71,6 +73,17 @@ export class PessoaService {
     return this.http.put(`${this.pessoasUrl}/${codigo}/ativo`, ativo, { headers })
       .toPromise()
       .then(() => null);
+  }
+
+  adicionar(pessoa: Pessoa): Promise<Pessoa> {
+    const headers = new Headers();
+    headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.post(this.pessoasUrl,
+        JSON.stringify(pessoa), { headers })
+      .toPromise()
+      .then(response => response.json());
   }
 
 }
