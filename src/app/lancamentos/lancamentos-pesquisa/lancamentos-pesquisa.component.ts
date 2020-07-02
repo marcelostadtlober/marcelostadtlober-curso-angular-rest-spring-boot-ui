f3d49753de242e6399607994624d9abb208260de
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-07-02 19:21:52 -0300

    Protegendo componentes

--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -4,6 +4,7 @@ import { Title } from '@angular/platform-browser';
 import { LazyLoadEvent, ConfirmationService } from 'primeng/components/common/api';
 import { ToastyService } from 'ng2-toasty';
 
+import { AuthService } from './../../seguranca/auth.service';
 import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
 import { ErrorHandlerService } from 'app/core/error-handler.service';
 
@@ -21,6 +22,7 @@ export class LancamentosPesquisaComponent implements OnInit {
 
   constructor(
       private lancamentoService: LancamentoService,
+      private auth: AuthService,
       private errorHandler: ErrorHandlerService,
       private toasty: ToastyService,
       private confirmation: ConfirmationService,
